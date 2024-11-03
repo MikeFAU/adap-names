59c5a45329cfa4d5d6ecfb0399136cb569c03e4d
oc83aroj <oc83aroj@fau.de>
2024-11-03 08:22:52 +0100

    Implementation of b02

--- src/adap-b02/names/StringArrayName.ts
+++ src/adap-b02/names/StringArrayName.ts
@@ -6,52 +6,88 @@ export class StringArrayName implements Name {
     protected delimiter: string = DEFAULT_DELIMITER;
     protected components: string[] = [];
 
-    constructor(source: string[], delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+    constructor(other: string[], delimiter?: string) {
+        this.components = other;
+        if (delimiter != null && delimiter != undefined){
+            this.delimiter = delimiter;
+        }
     }
 
+    /** Returns human-readable representation of Name instance */
+    /** @methodtype conversion-method */
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        return this.components.join(delimiter);
     }
 
+    /** @methodtype conversion-method */
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        return this.components.join(ESCAPE_CHARACTER+DEFAULT_DELIMITER);
     }
 
-    public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
-    }
-
+    /** @methodtype assertion-method */
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+        return this.components.length < 1;
     }
 
+    /** @methodtype get-method */
+    public getDelimiterCharacter(): string {
+        return this.delimiter;
+    }
+
+    /** Returns number of components in Name instance */
+    /** @methodtype get-method */
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.components.length;
     }
 
+    /** @methodtype get-method */
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        this.isValidRange(i);
+        return this.components[i];
     }
 
+    /** @methodtype set-method */
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.isValidRange(i);
+        this.components[i] = c;
     }
 
+    /** @methodtype command-method */
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.isValidRange(i);
+        this.components.splice(i, 0, c);
     }
 
+    /** @methodtype command-method */
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.splice(this.getNoComponents(), 0, c);
     }
 
+    /** @methodtype command-method */
     public remove(i: number): void {
-        throw new Error("needs implementation or deletion");
+        this.isValidRange(i);
+        this.components.splice(i, 1);
     }
 
+    /** @methodtype command-method */
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        this.isNotNone(other);
+
+        let otherComps = other.asDataString().split(ESCAPE_CHARACTER+DEFAULT_DELIMITER);
+        for (var comp of otherComps){
+            this.append(comp);
+        }
     }
 
+    /** @methodtype assertion-method */
+    protected isValidRange(i: number): void {
+        if (i < 0 || i >= this.components.length)
+            throw new RangeError("Out of Range");
+    }
+
+    /** @methodtype assertion-method */
+    protected isNotNone(other: Name): void {
+        if (other === null || other === undefined )
+            throw new TypeError("No Inputdata given");
+    }
 }
--- src/adap-b02/names/StringName.ts
+++ src/adap-b02/names/StringName.ts
@@ -7,52 +7,125 @@ export class StringName implements Name {
     protected name: string = "";
     protected noComponents: number = 0;
 
-    constructor(source: string, delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+    constructor(other: string, delimiter?: string) {
+        this.name = other;
+        if (delimiter != null && delimiter != undefined){
+            this.delimiter = delimiter;
+        }
+        this.name = this.name.replaceAll(this.delimiter, ESCAPE_CHARACTER+DEFAULT_DELIMITER);
     }
 
+    /** @methodtype conversion-method */
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        return this.name.replaceAll(ESCAPE_CHARACTER+DEFAULT_DELIMITER, delimiter);
     }
 
+    /** @methodtype conversion-method */
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        return this.name;
     }
 
-    public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
-    }
-
+    /** @methodtype assertion-method */
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+        return this.name.length < 1;
     }
 
+    /** @methodtype get-method */
+    public getDelimiterCharacter(): string {
+        return this.delimiter;
+    }
+
+    /** @methodtype get-method */
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.getComponents().length;
     }
 
+    /** @methodtype get-method */
     public getComponent(x: number): string {
-        throw new Error("needs implementation or deletion");
+        this.isValidRange(x);
+        return this.getComponents()[x];
     }
 
+    /** @methodtype set-method */
     public setComponent(n: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.isValidRange(n);
+        this.isNotNone(c);
+        let comps = this.getComponents();
+
+        // Change
+        comps[n] = c;
+
+        // Update
+        this.updateName(comps);
     }
 
+    /** @methodtype command-method */
     public insert(n: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.isValidRange(n);
+        this.isNotNone(c);
+
+        // Change
+        let comps = this.getComponents();
+        comps.splice(n, 0, c);
+
+        // Update
+        this.updateName(comps);
     }
 
+    /** @methodtype command-method */
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.isNotNone(c);
+
+        // Change
+        let comps = this.getComponents();
+        comps.splice(this.getNoComponents(), 0, c);
+
+        // Update
+        this.updateName(comps);
     }
 
+    /** @methodtype command-method */
     public remove(n: number): void {
-        throw new Error("needs implementation or deletion");
+        this.isValidRange(n);
+
+        // Change
+        let comps = this.getComponents();
+        comps.splice(n, 1);
+
+        // Update
+        this.updateName(comps);
     }
 
+    /** @methodtype command-method */
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        this.isNotNone(other);
+
+        let otherComps = other.asDataString().split(ESCAPE_CHARACTER+DEFAULT_DELIMITER);
+        for (var comp of otherComps){
+            this.append(comp);
+        }
     }
 
+    /** @methodtype get-method */
+    private getComponents(): string[] {
+        return this.name.split(ESCAPE_CHARACTER+DEFAULT_DELIMITER);
+    }
+
+    /** @methodtype helper-method */
+    private updateName(c: string[]): void {
+        let newName = c.join(ESCAPE_CHARACTER+DEFAULT_DELIMITER);
+        this.name = newName;
+    }
+
+    /** @methodtype assertion-method */
+    protected isValidRange(i: number): void {
+        if (i < 0 || i >= this.getComponents().length)
+            throw new RangeError("Out of Range");
+    }
+
+    /** @methodtype assertion-method */
+    protected isNotNone(other: Object): void {
+        if (other === null || other === undefined )
+            throw new TypeError("No Inputdata given");
+    }
 }
