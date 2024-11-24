159aafc04f64e9dfb7ea73369072c1b629fd9213
oc83aroj <oc83aroj@fau.de>
2024-11-24 02:47:16 +0100

    update implementation for B04 based on B03

--- src/adap-b04/names/AbstractName.ts
+++ src/adap-b04/names/AbstractName.ts
@@ -6,52 +6,132 @@ export abstract class AbstractName implements Name {
     protected delimiter: string = DEFAULT_DELIMITER;
 
     constructor(delimiter: string = DEFAULT_DELIMITER) {
-        throw new Error("needs implementation or deletion");
+        if (delimiter != null && delimiter != undefined){
+            this.delimiter = delimiter;
+        }
     }
 
+    /** @methodtype cloning-method */
     public clone(): Name {
-        throw new Error("needs implementation or deletion");
+        let n: Name = this.createEmptyNameWithEqualDelimiter();
+        n.concat(this);
+
+        return n;
     }
 
+    /** @methodtype conversion-method */
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        return this.getNameArray().join(delimiter).replaceAll(ESCAPE_CHARACTER+delimiter, delimiter);
+
     }
 
     public toString(): string {
+        // Method uses the asDataString representation
         return this.asDataString();
     }
 
+    /** @methodtype conversion-method */
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        return this.getNameArray().join(DEFAULT_DELIMITER);
     }
 
+    /** @methodtype assertion-method */
     public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
+        this.isNotNone(other);
+        
+        // Check if deliminiter is equal
+        if(this.getDelimiterCharacter() != other.getDelimiterCharacter())
+            return false;
+
+        // Check if number of components are equal
+        if(this.getNoComponents() != other.getNoComponents())
+            return false;
+
+        // Check if all single components are equal
+        for(let i = 0; i < this.getNoComponents(); i++){
+            if(this.getComponent(i) != other.getComponent(i))
+                return false;
+        }
+
+        // Everything is the same
+        return true;
     }
 
+    /** @methodtype get-method */
     public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
+        // See ADAP B01 slide 19f - Used adapted hash code implementation
+        
+        let hashCode: number = 0;
+        let c:number = 0;
+        const s: string = this.getNoComponents()+this.getDelimiterCharacter()+this.getNameArray().join(this.getDelimiterCharacter());
+        //console.info(s);
+
+        for (let i = 0; i < s.length; i++){
+            c = s.charCodeAt(i);
+            hashCode = (hashCode << 5) - hashCode + c;
+            hashCode |= 0;
+        }
+
+        return hashCode;
     }
 
+    /** @methodtype assertion-method */
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+        return this.getNoComponents() < 1;
     }
 
+    /** @methodtype get-method */
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+        return this.delimiter;
     }
 
+    /** @methodtype get-method */
     abstract getNoComponents(): number;
 
+    /** @methodtype get-method */
     abstract getComponent(i: number): string;
+    /** @methodtype set-method */
     abstract setComponent(i: number, c: string): void;
 
+    /** @methodtype command-method */
     abstract insert(i: number, c: string): void;
+    /** @methodtype command-method */
     abstract append(c: string): void;
+    /** @methodtype command-method */
     abstract remove(i: number): void;
 
+    /** @methodtype factory-method */
+    abstract createEmptyNameWithEqualDelimiter(): Name;
+
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        this.isNotNone(other);
+
+        for(let i = 0; i < other.getNoComponents(); i++){
+            this.append(other.getComponent(i));
+        }
     }
 
+    //#############################################################//
+    // Further methods added
+
+    /** @methodtype assertion-method */
+    protected isValidRange(i: number): void {
+        if (i < 0 || i >= this.getNoComponents())
+            throw new RangeError("Out of Range");
+    }
+
+    /** @methodtype assertion-method */
+    protected isNotNone(other: Object): void {
+        if (other === null || other === undefined )
+            throw new TypeError("No Inputdata given");
+    }
+
+    private getNameArray(): string[] {
+        let retArr:string[] = [];
+        
+        for(let i = 0; i < this.getNoComponents(); i++)
+            retArr.push(this.getComponent(i));
+
+        return retArr;
+    }
 }
--- src/adap-b04/names/StringName.ts
+++ src/adap-b04/names/StringName.ts
@@ -7,65 +7,112 @@ export class StringName extends AbstractName {
     protected name: string = "";
     protected noComponents: number = 0;
 
-    constructor(source: string, delimiter?: string) {
-        super();
-        throw new Error("needs implementation or deletion");
-    }
+    constructor(other: string, delimiter?: string) {
+        super(delimiter);
 
-    public clone(): Name {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public asDataString(): string {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+        this.isNotNone(other);
+        this.name = other;
     }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.createArrayByDelimiter(this.name).length;
     }
 
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        this.isValidRange(i);
+        return this.createArrayByDelimiter(this.name)[i];
     }
 
     public setComponent(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+        this.isValidRange(i);
+        this.isNotNone(c);
+        let comps = this.createArrayByDelimiter(this.name);
+
+        // Change
+        comps[i] = c;
+
+        // Update
+        this.updateName(comps);
     }
 
     public insert(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+        this.isValidRange(i);
+        this.isNotNone(c);
+
+        // Change
+        let comps = this.createArrayByDelimiter(this.name);
+        comps.splice(i, 0, c);
+
+        // Update
+        this.updateName(comps);
     }
 
     public append(c: string) {
-        throw new Error("needs implementation or deletion");
+        this.isNotNone(c);
+
+        // Change
+        let comps = this.createArrayByDelimiter(this.name);
+        comps.splice(this.getNoComponents(), 0, c);
+
+        // Update
+        this.updateName(comps);
     }
 
     public remove(i: number) {
-        throw new Error("needs implementation or deletion");
+        this.isValidRange(i);
+
+        // Change
+        let comps = this.createArrayByDelimiter(this.name);
+        comps.splice(i, 1);
+
+        // Update
+        this.updateName(comps);
     }
 
-    public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+    createEmptyNameWithEqualDelimiter(): Name {
+        return new StringName("", this.getDelimiterCharacter());
     }
 
+    //#############################################################//
+    // Further methods added
+
+    /** @methodtype helper-method */
+    private createArrayByDelimiter(inputName: String): string[] {
+        
+        let retArr:string[] = [];
+        let tempStr:string = "";
+        let lastChar:string = "";
+        for (let i = 0; i < inputName.length; i++){
+            let add = true;
+            // Check if current char is a control character that is not escaped
+            if (inputName[i] === this.delimiter && lastChar != ESCAPE_CHARACTER){
+                retArr.push(tempStr);
+                tempStr = "";
+
+                // Do not add control characters to temp string
+                add = false;
+            }
+
+            // Append to tempStr
+            if (add){
+                tempStr = tempStr + inputName[i];
+            }
+
+            // Always remember last char to check for escape char
+            lastChar = inputName[i];
+
+            // Edge Case: Last char
+            if (i === inputName.length -1 ){
+                retArr.push(tempStr);
+            }
+        }
+
+        return retArr;
+    }
+
+    /** @methodtype helper-method */
+    private updateName(c: string[]): void {
+        let newName = c.join(this.delimiter);
+        this.name = newName;
+    }
 }
