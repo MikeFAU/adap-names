ce4070ff61ff905995aff8554d9fbd65a440aeb4
oc83aroj <oc83aroj@fau.de>
2024-11-30 10:47:39 +0100

    Added B04 implementation of files to B05

--- src/adap-b05/files/File.ts
+++ src/adap-b05/files/File.ts
@@ -1,6 +1,8 @@
 import { Node } from "./Node";
 import { Directory } from "./Directory";
 import { MethodFailedException } from "../common/MethodFailedException";
+import { InvalidStateException } from "../common/InvalidStateException";
+
 
 enum FileState {
     OPEN,
@@ -11,6 +13,7 @@ enum FileState {
 export class File extends Node {
 
     protected state: FileState = FileState.CLOSED;
+    protected perms: number = 0;
 
     constructor(baseName: string, parent: Directory) {
         super(baseName, parent);
@@ -18,9 +21,13 @@ export class File extends Node {
 
     public open(): void {
         // do something
+        this.assertIsInState(FileState.CLOSED);
+        this.doSetFileState(FileState.OPEN);
     }
 
     public read(noBytes: number): Int8Array {
+        this.assertIsInState(FileState.OPEN);
+
         let result: Int8Array = new Int8Array(noBytes);
         // do something
 
@@ -40,15 +47,49 @@ export class File extends Node {
     }
 
     protected readNextByte(): number {
+        this.assertIsInState(FileState.OPEN);
         return 0; // @todo
     }
 
     public close(): void {
         // do something
+        this.assertIsInState(FileState.OPEN);
+        this.doSetFileState(FileState.CLOSED);
+    }
+
+    public write(o: Object[]): void {
+        this.assertIsInState(FileState.OPEN);
+        // do something 
+    }
+
+    public getPerms(): number {
+        return this.doGetPerms();
+    }
+
+    public setPerms(p: number): void {
+        // do something
+    }
+
+    public delete(): void{
+        this.assertIsInState(FileState.CLOSED);
+        this.doSetFileState(FileState.DELETED);
     }
 
     protected doGetFileState(): FileState {
         return this.state;
     }
 
+    protected doSetFileState(state: FileState): void{
+        this.state = state;
+    }
+
+    protected doGetPerms(): number {
+        return this.perms;
+    }
+
+    protected assertIsInState(state: FileState){
+        if (state != this.doGetFileState()){
+            throw new InvalidStateException("invalid file state");
+        }
+    }
 }
---
+++ test/adap-b05/files/B04Files.test.ts
@@ -0,0 +1,96 @@
+import { describe, it, expect } from "vitest";
+
+import { IllegalArgumentException } from "../../../src/adap-b05/common/IllegalArgumentException";
+import { MethodFailedException } from "../../../src/adap-b05/common/MethodFailedException";
+import { InvalidStateException } from "../../../src/adap-b05/common/InvalidStateException";
+
+import {Directory} from "../../../src/adap-b05/files/Directory"
+import {File} from "../../../src/adap-b05/files/File"
+import {Link} from "../../../src/adap-b05/files/Link"
+import {Node} from "../../../src/adap-b05/files/Node"
+import {RootNode} from "../../../src/adap-b05/files/RootNode"
+
+describe("Client-File Contract according to ADAP B04 Slide 10", () => {
+    it("test don't open an open file", async () => {
+        let rootNode:RootNode = new RootNode();
+        let usr:Directory = new Directory("usr", rootNode);
+        let bin:Directory = new Directory("bin", usr);
+        let n:File = new File("ls", bin);
+
+        n.open();
+        expect(() => n.open()).toThrow(new InvalidStateException("invalid file state"));
+    });
+
+    it("test don't open a deleted file", async () => {
+        let rootNode:RootNode = new RootNode();
+        let usr:Directory = new Directory("usr", rootNode);
+        let bin:Directory = new Directory("bin", usr);
+        let n:File = new File("ls", bin);
+
+        n.delete();
+        expect(() => n.open()).toThrow(new InvalidStateException("invalid file state"));
+    });
+
+    it("test don't close a closed file", async () => {
+        let rootNode:RootNode = new RootNode();
+        let usr:Directory = new Directory("usr", rootNode);
+        let bin:Directory = new Directory("bin", usr);
+        let n:File = new File("ls", bin);
+
+        n.open();
+        n.close();
+        expect(() => n.close()).toThrow(new InvalidStateException("invalid file state"));
+    });
+
+    it("test don't close a deleted file", async () => {
+        let rootNode:RootNode = new RootNode();
+        let usr:Directory = new Directory("usr", rootNode);
+        let bin:Directory = new Directory("bin", usr);
+        let n:File = new File("ls", bin);
+
+        n.delete();
+        expect(() => n.close()).toThrow(new InvalidStateException("invalid file state"));
+    });
+
+    it("test don't read from a closed file", async () => {
+        let rootNode:RootNode = new RootNode();
+        let usr:Directory = new Directory("usr", rootNode);
+        let bin:Directory = new Directory("bin", usr);
+        let n:File = new File("ls", bin);
+
+        n.open();
+        n.close();
+        expect(() => n.read(10)).toThrow(new InvalidStateException("invalid file state"));
+    });
+
+    it("test don't read from a deleted file", async () => {
+        let rootNode:RootNode = new RootNode();
+        let usr:Directory = new Directory("usr", rootNode);
+        let bin:Directory = new Directory("bin", usr);
+        let n:File = new File("ls", bin);
+
+        n.delete();
+        expect(() => n.read(10)).toThrow(new InvalidStateException("invalid file state"));
+    });
+
+    it("test don't write to a closed file", async () => {
+        let rootNode:RootNode = new RootNode();
+        let usr:Directory = new Directory("usr", rootNode);
+        let bin:Directory = new Directory("bin", usr);
+        let n:File = new File("ls", bin);
+
+        n.open();
+        n.close();
+        expect(() => n.write([])).toThrow(new InvalidStateException("invalid file state"));
+    });
+
+    it("test don't write to a deleted file", async () => {
+        let rootNode:RootNode = new RootNode();
+        let usr:Directory = new Directory("usr", rootNode);
+        let bin:Directory = new Directory("bin", usr);
+        let n:File = new File("ls", bin);
+
+        n.delete();
+        expect(() => n.write([])).toThrow(new InvalidStateException("invalid file state"));
+    });
+  });
